7fd8115afc11532e08efeb02d7271c3f1e622fd0
Farhad Eidi <farcom7@gmail.com>
2020-07-21 23:07:11 +0300

    Update One - Just for help

--- index.android.js
+++ index.android.js
@@ -5,17 +5,15 @@
 import { NativeModules, requireNativeComponent } from 'react-native';
 
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
-export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
+export const JitsiMeetModule = NativeModules.RNJitsiMeetModule;
 const call = JitsiMeetModule.call;
 const audioCall = JitsiMeetModule.audioCall;
-JitsiMeetModule.call = (url, userInfo) => {
+JitsiMeetModule.call = (url, userInfo, meetOptions, meetFeatureFlags) => {
   userInfo = userInfo || {};
-  call(url, userInfo);
-}
+  call(url, userInfo, meetOptions, meetFeatureFlags);
+};
 JitsiMeetModule.audioCall = (url, userInfo) => {
   userInfo = userInfo || {};
   audioCall(url, userInfo);
-}
+};
 export default JitsiMeetModule;
-
-
